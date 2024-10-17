c383219f056d8b5afcd977c7a0430c2ca415827e
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2024-10-17 14:35:46 +0200

    modified:   APIs/API.js

--- APIs/API.js
+++ APIs/API.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 const api = "http://10.2.47.80:3001/api"; //make sure you use your own ip address here. But leave the port number
 
-export default api;
+export default api; 
